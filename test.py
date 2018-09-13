26437b3af1ce4ba0a80239355c4cd3a932324012
wildcard329 <42948540+wildcard329@users.noreply.github.com>
2018-09-12 23:49:38 -0700

    Update test.py
    I probably could have done a little more to make my code work better, but I only have twenty attempts per exercise, so I didn't want to go too overboard.

--- test.py
+++ test.py
@@ -4,22 +4,47 @@
 # False statements to see if that is what it was looking for, otherwise I don't
 # know what to do.
 
-hours_worked = int(input())
-if hours_worked < 0 or hours_worked > 137:
-	print('INVALID')
-	hours_worked = int(input())
+# I successfully completed the assignment. I kept toying around with complexity, but 
+# in the end was able to settle on a simple solution.
 
-pay = hours_worked 
+#hours_worked = int(input())
+#if hours_worked < 0 or hours_worked > 137:
+#	print('INVALID')
+#	hours_worked = int(input())
 
-if hours_worked <= 40:
-	pay = hours_worked * 8
-elif hours_worked <= 50:
-	pay = ((hours_worked - 40) * 9) + (40*8)
-elif hours_worked < 168:
-	pay = ((hours_worked - 50) * 10) + (40*8) + (10*9)
+#pay = hours_worked 
+
+#if hours_worked <= 40:
+
+#	pay = hours_worked * 8
+#elif hours_worked <= 50:
+#	pay = ((hours_worked - 40) * 9) + (40*8)
+#elif hours_worked < 168:
+#	pay = ((hours_worked - 50) * 10) + (40*8) + (10*9)
+#else:
+#	print('INVALID')
+#print('YOU MADE', pay, 'DOLLARS THIS WEEK')
+
+hours = int(input())
+M = 'YOU MADE '
+W = ' DOLLARS THIS WEEK'
+
+check = True
+
+if hours >= 0 and hours <= 168:
+	check = True
 else:
+	check = False
 	print('INVALID')
-print('YOU MADE', pay, 'DOLLARS THIS WEEK')
-
-
-
+	
+if check == True and hours >= 0 and hours <= 40:
+	pay = hours * 8
+	print(M + str(pay) + W)
+elif check == True and hours > 40 and hours <= 50:
+	pay = ((hours - 40) * 9) + (40*8)
+	print(M + str(pay) + W)
+elif check == True and hours > 50:
+	pay = ((hours - 50) * 10) + (40*8) + (10*9)
+	print(M + str(pay) + W)
+else:
+	pass
