82e8d0cf018c372d327163c1daef60e9fe5715cf
wildcard329 <42948540+wildcard329@users.noreply.github.com>
2018-09-26 15:29:18 -0700

    Update prjctealer1.py

--- prjctealer1.py
+++ prjctealer1.py
@@ -1,8 +1,9 @@
 # Solution in python to first scenario on project euler. 
+# Project called for solution for the sum of the multiples of 3 and 5 below 1000.
 # In Python, multiples can be added through use of the % operator.
 # In this scenario, modulo 3 and 5 were used to get the sum, but because
 # modulo 15 got counted twice as a result of this method, after the script
-# runs we add var_3 to var_5 and subtract var_15.
+# runs we add var_3 to var_5 and subtract var_15 (which only counted modulo 15 once).
 
 counter = 0
 var_3 = 0
