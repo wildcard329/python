8ec839768c253169b0c4384b7d91a930cff3d7b5
danbergh412 <danbergh412@gmail.com>
2018-09-25 21:13:42 -0700

    Added Dan's version of time converter

---
+++ time_conversion_calc_dan.py
@@ -0,0 +1,30 @@
+import sys
+
+class Time:
+    def __init__(self, totalSeconds):
+        secondsToDays = (60 * 60 * 24);
+        secondsToHours = (60 * 60);
+        secondsToMinutes = 60;
+
+        self.days = totalSeconds // secondsToDays
+        totalSeconds = totalSeconds % secondsToDays
+
+        self.hours = totalSeconds // secondsToHours
+        totalSeconds = totalSeconds % secondsToHours
+
+        self.minutes = totalSeconds // secondsToMinutes
+
+        self.seconds = totalSeconds % secondsToMinutes
+    def __str__(self):
+        return str(self.days) + ' days ' + str(self.hours) + ' hours ' + str(self.minutes) + ' minutes ' + str(self.seconds) + ' seconds'
+
+
+time = Time(200);
+
+if len(sys.argv) > 1:
+    seconds = sys.argv[1]
+else:
+    seconds = input("Enter time in seconds:")
+seconds = int(seconds)
+
+print(Time(seconds))
