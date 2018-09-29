9ccea2ffd0c947be71cad7256b648b49c7a18913
danbergh412 <danbergh412@gmail.com>
2018-09-29 09:06:16 -0700

    Added basic crossword puzzle

---
+++ crossword.py
@@ -0,0 +1,31 @@
+word = "Test word."
+
+def changeHiddenWord(hiddenWord, word, letter):
+    newHiddenWord = ""
+    for i in range(0, len(hiddenWord)):
+        if word[i].lower() == letter.lower():
+            newHiddenWord += word[i]
+        else:
+            newHiddenWord += hiddenWord[i]
+    return newHiddenWord
+
+def hideWord(word):
+    hiddenWord = ""
+    for c in word:
+        if c.isalpha():
+            hiddenWord += '_'
+        else:
+            hiddenWord += c
+    return hiddenWord
+
+def startGame(word):
+    hiddenWord = hideWord(word)
+
+    print(hiddenWord)
+    while hiddenWord != word:
+        letter = input("Enter letter")
+        hiddenWord = changeHiddenWord(hiddenWord, word, letter)
+        print(hiddenWord)
+
+
+startGame(word)
