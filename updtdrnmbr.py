9ebbc299ab5b89a51d47b6fcde82964a0c38942f
wildcard329 <42948540+wildcard329@users.noreply.github.com>
2018-11-02 14:51:37 -0700

    Update updtdrnmbr.py

--- updtdrnmbr.py
+++ updtdrnmbr.py
@@ -8,7 +8,7 @@ guesses = []
 
 counter = 1
 
-while guess != rNumber:
+while guess != rNumber and counter < 10:
   guess = int(input('Guess the random number between 1 and 100. '))
   if guess != rNumber:
     guesses += str(int(guess))
@@ -18,9 +18,6 @@ while guess != rNumber:
     print('Higher')
   elif guess > rNumber:
     print('Lower')
-  if counter == 10:
-    print('Oh no! You are out of attempts!')
-    break
     
 if guess == rNumber:
   print('Congradulations! That was the number! Attempts: ',counter)
